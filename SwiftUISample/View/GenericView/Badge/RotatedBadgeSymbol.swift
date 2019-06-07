28414403be1945195356c6fa10451c88f83eec0f
Amir Daliri <amir.daliri69@gmail.com>
2019-06-07 18:54:47 +0300

    add badge method and Hexagon Parameters

---
+++ SwiftUISample/View/GenericView/Badge/RotatedBadgeSymbol.swift
@@ -0,0 +1,27 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwiftUISample
+//
+//  Created by Amir Daliri on 6/7/19.
+//  Copyright © 2019 Amir Daliri. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#if DEBUG
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
+#endif
